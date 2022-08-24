4c97858341bb2de1e23eaf69ee63ed8a585de873
sLrBastos <cerejobastosa7x@gmail.com>
2022-08-24 18:08:23 +0100

    2nd commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const QUOTE_API_URL = 'http://api.quotable.io/random'
+const quoteDisplayElement = document.getElementById("quoteDisplay")
+const quoteInputElement = document.getElementById("quoteInput")
+const timerElement = document.getElementById("timer")
+
+quoteInputElement.addEventListener("input", () => {
+    const arrayQuote = quoteDisplayElement.querySelectorAll("span")
+    const arrayValue = quoteInputElement.value.split("")
+    let correct = true
+    arrayQuote.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove("correct")
+            characterSpan.classList.remove("incorrect")
+            correct = false
+        }
+        else if (character === characterSpan.innerText) {
+            characterSpan.classList.add("correct")
+            characterSpan.classList.remove("incorrect")
+        } else {
+            characterSpan.classList.remove("correct")
+            characterSpan.classList.add("incorrect")
+            correct = false
+        }
+    })
+     if (correct) renderNew()
+})
+
+
+function getQuote() {
+    return fetch(QUOTE_API_URL)
+        .then(response => response.json())
+        .then(data => data.content)
+}
+
+async function renderNew() {
+    const quote = await getQuote()
+    quoteDisplayElement.innerHTML = ""
+    quote.split("").forEach(character => {
+        const characterSpan = document.createElement("span")   
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+    });
+    quoteInputElement.value = null
+    Timer()
+}
+
+let startTime
+function Timer() {
+    timerElement.innerText = 0
+    startTime = new Date()
+    setInterval(() => {
+        timer.innerText = getTimerTime()
+    }, 1000)
+}
+
+function getTimerTime() {
+    return Math.floor((new Date() - startTime) /1000)
+}
+
+renderNew()
